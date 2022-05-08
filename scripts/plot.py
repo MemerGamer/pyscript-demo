9e6a6510ca53c97cdff218afeb4dd03ffdb0ab34
Kovács Bálint Hunor <kovacsbalinthunor53@gmail.com>
2022-05-08 19:05:54 +0300

    trying out plotting

---
+++ scripts/plot.py
@@ -0,0 +1,28 @@
+
+from matplotlib.patches import Polygon
+import matplotlib.pyplot as plt #fgv abrazolashoz szukseges
+import numpy as np #vektormuveletekhez szukseges csomag
+import math as m
+
+def szabalyos(n):
+    radius = 0.5
+    rotation=0
+    translation=None
+    one_segment = m.pi * 2 / n
+
+    points = [
+        (m.sin(one_segment * i + rotation) * radius,
+         m.cos(one_segment * i + rotation) * radius)
+        for i in range(n+1)]
+
+    if translation:
+        points = [[sum(pair) for pair in zip(point, translation)]
+                  for point in points]
+        
+    xs, ys = zip(*points)
+    
+    plt.figure()
+    plt.axis('equal')
+    plt.plot(xs,ys)
+    plt.show()
+
