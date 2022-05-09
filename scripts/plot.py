600fb5e00a286f0cd5450e8117ff29c20b676f49
Kovács Bálint Hunor <kovacsbalinthunor53@gmail.com>
2022-05-09 21:34:43 +0300

    working on user input and plotting

--- scripts/plot.py
+++ scripts/plot.py
@@ -1,6 +1,6 @@
 import matplotlib.pyplot as plt
 import math as m
-def szabalyos(n):
+'''def szabalyos(n):
     radius = 0.5
     rotation=0
     translation=None
@@ -21,3 +21,22 @@ def szabalyos(n):
     plt.axis('equal')
     plt.plot(xs,ys)
     plt.show()
+szabalyos(4)'''
+
+fig, ax = plt.subplots()
+
+year_1 = [2016, 2017, 2018, 2019, 2020, 2021]
+population_1 = [42, 43, 45, 47, 48, 50]
+
+year_2 = [2016, 2017, 2018, 2019, 2020, 2021]
+population_2 = [43, 43, 44, 44, 45, 45]
+
+plt.plot(year_1, population_1, marker='o', linestyle='--', color='g', label='Country 1')
+plt.plot(year_2, population_2,  marker='d', linestyle='-', color='r', label='Country 2')
+
+plt.xlabel('Year')
+plt.ylabel('Population (M)')
+plt.title('Year vs Population')
+plt.legend(loc='lower right')
+plt.show()
+fig
