cdf47432f5559d936cb195e65824765618d49a7b
Kovács Bálint Hunor <kovacsbalinthunor53@gmail.com>
2022-05-10 20:40:53 +0300

    code cleanup for faster loading time

--- scripts/volume.py
+++ scripts/volume.py
@@ -1,33 +1,32 @@
-import math as m
-
-import numpy as np
+from ast import excepthandler
+from numpy import cross, dot
 
 from js import document,alert
      
-def getVect(A,B):
+def get_vector(A,B):
     V = ()
     for i in range(len(A)):
         V = V + (B[i]-A[i],)
     return V
 
-def Terfogat(A,B,C,D):
-    vAB = getVect(A,B)
-    vAC = getVect(A,C)
-    vAD = getVect(A,D)
-    
-    ABxAC = np.cross(vAB,vAC)
-    #print(ABxAC)
-    nevezo = np.dot(ABxAC,vAD)
-    V = nevezo / 6
-    return V
+def calculate_volume(A,B,C,D):
+    try:
+        vAB, vAC, vAD = get_vector(A,B), get_vector(A,C), get_vector(A,D)   
+        ABxAC = cross(vAB,vAC)
+        return (dot(ABxAC,vAD) / 6)
+    except:
+        raise Exception("Invalid Data")
     
 def convert(*ags, **kws):
     
-    A = document.getElementById('coordinate1').value;
-    B = document.getElementById('coordinate2').value;
-    C = document.getElementById('coordinate3').value;
-    D = document.getElementById('coordinate4').value;
+    try:
+        A = document.getElementById('coordinate1').value;
+        B = document.getElementById('coordinate2').value;
+        C = document.getElementById('coordinate3').value;
+        D = document.getElementById('coordinate4').value;
     
-    result=Terfogat(eval(A),eval(B),eval(C),eval(D))
-    result = abs(result)
-    pyscript.write("result",result)    
+    
+        result=abs(calculate_volume(eval(A),eval(B),eval(C),eval(D)))
+        pyscript.write("result",result)    
+    except:
+        pyscript.write("result","Invalid INPUT")    
